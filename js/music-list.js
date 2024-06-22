f9facaed5c2fb504ee7ac0ed2a5f43aea331dacc
Boweii22 <tombribowei01@gmail.com>
2024-06-22 10:09:39 +0100

    Music Player Web v1

---
+++ js/music-list.js
@@ -0,0 +1,41 @@
+let allMusic = [
+
+    {
+        name: "Hmmm (ft. Davido)",
+        artist: "Chris Brown, Davido",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Anni3 – Jekasoro",
+        artist: "Anni3",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Dave & Ruelle - Lesley",
+        artist: "Dave & Ruelle",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Jordan Adetunji - KEHLANI",
+        artist: "Jordan Adetunji",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "One Of The Girls (with JENNIE, Lily Rose Depp)",
+        artist: "The Weeknd",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Victony - Risk",
+        artist: "Victony",
+        img: "music-6",
+        src: "music-6"
+    },
+    
+    ];
+    
